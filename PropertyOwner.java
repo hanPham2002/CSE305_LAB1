6097c893e864da0e23bdc41cd85aa1591c76e57c
hanPham2002 <phambaohan160402@gmail.com>
2025-04-26 10:27:17 +0700

    Create PropertyOwner.java

---
+++ PropertyOwner.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PropertyOwner {
+    private User user;
+    private List<String> listOfProperties;
+
+    public PropertyOwner(User user) {
+        this.user = user;
+        this.listOfProperties = new ArrayList<>();
+    }
+
+    public void addProperty(String property) {
+   
+    }
+
+    public void removeProperty(String property) {
+    
+    }
+}
